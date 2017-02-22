a0c5ed321fb9e2d57b10da5dc49458634109b2cc
naldin <ronaldo.rrj@gmail.com>
2017-02-22 13:41:20 -0300

    Version v0.3

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -5,56 +5,50 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 Servo myservoR;
 Servo myservoL;
 
-char readserialL;
-char readserialR;
-String strValL;
-String strValR;
-int intValL;
-int intValR;
+char readserial;
+String strVal;
+int intVal;
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
   pinMode(6, OUTPUT);  //LCD pin setup
   analogWrite(6, 255); //LCD brightness
-  myservoR.attach(7);
-  myservoL.attach(A0);
+  myservoL.attach(7);
+  myservoR.attach(A0);
+  myservoL.write(120);
+  myservoR.write(45);
 }
 
 void loop() {
   if (Serial.available()) {
     lcd.clear();
     delay(5);
-    if (Serial.read() == 'l') {
-      while (Serial.available() > 0) {
-        readserialL = Serial.read(); // recebe cada caracter da serial
-        strValL += readserialL; // carrega buffer string
-      }
 
-      // strVal.remove(0, 1);
-      intValL = (strValL.toInt());
-      intValL = map(intValL, 0, 999, 0, 120);
-      myservoL.write(intValL);
-      lcd.print("l ");
-      lcd.print(strValL);
-      strValL = "";
+    if (Serial.read() == 'r') {
+      while (Serial.available() > 0) {
+        readserial = Serial.read(); // recebe cada caracter da serial
+        strVal += readserial; // carrega buffer string
+      }
+      intVal = (strVal.toInt());
+      intVal = map(intVal, 0, 999, 45, 0);
+      myservoR.write(intVal);
+      lcd.print("R: ");
+      lcd.print(strVal);
+      strVal = "";
     }
-    
+
     else {
       while (Serial.available() > 0) {
-        readserialR = Serial.read(); // recebe cada caracter da serial
-        strValR += readserialR; // carrega buffer string
+        readserial = Serial.read(); // recebe cada caracter da serial
+        strVal += readserial; // carrega buffer string
       }
-
-      //strVal.remove(0, 1);
-      intValR = (strValR.toInt());
-      intValR = map(intValR, 0, 999, 0, 120);
-      myservoR.write(intValR);
-      lcd.print("r ");
-      lcd.print(strValR);
-      strValR = "";
+      intVal = (strVal.toInt());
+      intVal = map(intVal, 0, 999, 120, 180);
+      myservoL.write(intVal);
+      lcd.print("L: ");
+      lcd.print(strVal);
+      strVal = "";
     }
-
-
   }
 }
--- SimArduino.cpp
+++ SimArduino.cpp
@@ -4,10 +4,8 @@
 #include<time.h>
 #include "sharedmemory.h"
 #include "SerialClass.h"
-// Name of the pCars memory mapped file
-// Nome para memoria mapeada
-#define MAP_OBJECT_NAME "$pcars$"
-#define ANGLE 57.30 //Valor de conversão radianos para graus
+#define MAP_OBJECT_NAME "$pcars$" //Name of the pCars memory mapped file
+#define ANGLE 57.30 //Factor radians to degrees
 
 using namespace std;
 
@@ -15,12 +13,14 @@ using namespace std;
 char port[] = "COM5";
 Serial *SP = new Serial(port);
 
+// Print Values on screen
 void printValue(string label, float value)
 {
     cout.precision(6);
     cout << label << value << endl;
 }
 
+// Print RMP VU on screen
 void printVu(float vuRpm, float vuMrpm)
 {
     int max = 40;
@@ -44,21 +44,19 @@ void printVu(float vuRpm, float vuMrpm)
     cout << vuShow << endl;
 }
 
+// Running values on screen and send data to Arduino
 int runValues()
 {
-// *** Memory-mapped game ***  *** Mapeando a memoria do jogo ***
+// *** Memory-mapped game ***
 
 // Open the memory-mapped file
-// Abrindo o arquivo de memoria mapeada do jogo
     HANDLE fileHandle = OpenFileMapping( PAGE_READONLY, FALSE, MAP_OBJECT_NAME );
     if (fileHandle == NULL)
     {
         printf( "Could not open file mapping object %(d).\n", GetLastError() );
         return 1;
     }
-
 // Get the data structure
-// Buscando estrutura de dados
     const SharedMemory *sharedData = (SharedMemory*)MapViewOfFile( fileHandle, PAGE_READONLY, 0, 0, sizeof(SharedMemory) );
     if (sharedData == NULL)
     {
@@ -69,7 +67,6 @@ int runValues()
     }
 
 // Ensure we're sync'd to the correct data version
-// Verificando se a versão está  correta
     if ( sharedData->mVersion != SHARED_MEMORY_VERSION )
     {
         printf( "Data version mismatch\n");
@@ -77,11 +74,9 @@ int runValues()
     }
 
 // *** End memory-mapped ***
-// *** Fim do mapeamento de memoria ***
-
     // Variables
-    char outData[5];
-    char intChr[4];
+    char outData[20]; // If number [20] is to low will be crash
+    char intChr[20];
 
     float speed = sharedData->mSpeed;
     float rpm = sharedData->mRpm;
@@ -104,13 +99,12 @@ int runValues()
     cout << endl;
 
     // Send data to Arduino
-    // Enviando dados para Arduino
     if (angularVelocity > 0)
     {
         sprintf(intChr, "%.0f", angularVelocity*820);
         sprintf(outData, "%c", 'l');
         strcat(outData, intChr);
-        SP->WriteData(outData, (unsigned)strlen(outData));
+        SP->WriteData(outData, 4);
 
     }
     else if (angularVelocity < 0)
@@ -118,9 +112,9 @@ int runValues()
         sprintf(intChr, "%.0f", angularVelocity*-820);
         sprintf(outData, "%c", 'r');
         strcat(outData, intChr);
-        SP->WriteData(outData, (unsigned)strlen(outData));
+        SP->WriteData(outData, 4);
     }
-    Sleep(5);
+    Sleep(5); // Same time with Arduino to sync
     system("cls");
     return 0;
 }
